b0bed62d1b7a00e882ed5cf3eb442901f0fe8a1f
尼古拉斯丶少 <sean.snow@live.com>
2021-11-22 09:44:48 +0800

    feat: 关闭文件访问权限配置

--- src/main/java/tech/aomi/osshub/constant/Common.java
+++ src/main/java/tech/aomi/osshub/constant/Common.java
@@ -12,7 +12,7 @@ public class Common {
 
     public static List<Permission> ALL_PERMISSION = Arrays.asList(
             Permission.get(new String[]{"/files"}, new Authority[]{Authority.FILE_ALL, Authority.FILE_BROWSE}),
-            Permission.get(new String[]{"/files/{id}"}, new Authority[]{Authority.FILE_ALL, Authority.FILE_READ}),
+//            Permission.get(new String[]{"/files/{id}"}, new Authority[]{Authority.FILE_ALL, Authority.FILE_READ}),
             Permission.post(new String[]{"/files/**"}, new Authority[]{Authority.FILE_ALL, Authority.FILE_WRITE}),
             Permission.delete(new String[]{"/files/**"}, new Authority[]{Authority.FILE_ALL, Authority.FILE_WRITE})
     );
